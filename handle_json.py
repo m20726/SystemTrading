7e5f069177c2d0fbe73c8a56c1fb15d161702af4
m20726 <m20726@naver.com>
2023-04-10 16:42:16 +0900

    20이평선 처리 소스 분리

--- handle_json.py
+++ handle_json.py
@@ -13,11 +13,6 @@ def write_json_file(data, file_path):
         json.dump(data, file, indent=4)
 
 
-# stock_list = [
-#     {'name':'삼성전자', 'code':'005930', 'envelope_p':10, 'sell_target_p':7},
-#     {'name':'셀트리온', 'code':'068270', 'envelope_p':8, 'sell_target_p':11}
-# ]
-
 # write_json_file(stock_list, stock_list_file_path)
 # stock_list = read_json_file(stock_list_file_path)
 # print(stock_list)
