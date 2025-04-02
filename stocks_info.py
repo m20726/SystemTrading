d2e8cd65e9aa583ec2aca5b693e2da5869ae3553
sy.moon <sy.moon@kohyoung.com>
2025-04-02 10:21:29 +0900

    장중 익절,손절 처리 때문에 목표가 이상인 경우 매도 처리

--- stocks_info.py
+++ stocks_info.py
@@ -2289,27 +2289,33 @@ class Stocks_info:
                                     else:
                                         PRINT_DEBUG(f"[{self.stocks[code]['name']}] 매도 주문, {qty}주 {curr_price}(현재가) <= {take_profit_price}(익절가) {self.stocks[code]['highest_price_ever']}(최고가)")
                     else:
-                        # 목표가에 바로 매도 주문(not 트레일링 스탑)
-                        if self.already_ordered(code, SELL_CODE) == False:
+                        # 목표가 매도 처리(not 트레일링 스탑)
+                        # 장중 익절,손절 처리 때문에 목표가 이상인 경우 매도 처리
+                        if curr_price >= sell_target_price and sell_target_price > 0:
                             self.stocks[code]['allow_monitoring_sell'] = True
+                            self.stocks[code]['status'] = "매도 모니터링"
                             qty = self.get_sell_qty(code)
-                            # 주문 완료 했으면 다시 주문하지 않는다                        
-                            if self.sell(code, sell_target_price, qty, ORDER_TYPE_LIMIT_ORDER) == True:
+                            # 지정가 매도
+                            # 주문 완료 했으면 다시 주문하지 않는다
+                            if self.already_ordered(code, SELL_CODE) == False and self.sell(code, curr_price, qty, ORDER_TYPE_LIMIT_ORDER) == True:
                                 self.set_order_done(code, SELL_CODE)
-                                PRINT_DEBUG(f"[{self.stocks[code]['name']}] 매도 주문, {qty}주 {curr_price}(현재가) {sell_target_price}(목표가)")
+                                PRINT_DEBUG(f"[{self.stocks[code]['name']}] 매도 주문, {qty}주 {curr_price}(현재가) >= {sell_target_price}(목표가)")                        
                 else:
                     # 반 매도된 상태
                     # N차 매도가 : N-1차 매도가 * x (N>=2)
                     if self.trade_strategy.take_profit_strategy == TAKE_PROFIT_STRATEGY_SLOW:
                         # "현재가 >= 목표가" 경우 매도
                         # 익절은 handle_loss_cut 에서 처리
-                        if self.already_ordered(code, SELL_CODE) == False:
+                        # 장중 익절,손절 처리 때문에 목표가 이상인 경우 매도 처리
+                        if curr_price >= sell_target_price and sell_target_price > 0:
+                            # 1차 매도 후 다음날 매도 가능하게
                             self.stocks[code]['allow_monitoring_sell'] = True
+                            self.stocks[code]['status'] = "매도 모니터링"
                             qty = self.get_sell_qty(code)
                             # 주문 완료 했으면 다시 주문하지 않는다
-                            if self.sell(code, sell_target_price, qty, ORDER_TYPE_LIMIT_ORDER) == True:
+                            if self.already_ordered(code, SELL_CODE) == False and self.sell(code, curr_price, qty, ORDER_TYPE_LIMIT_ORDER) == True:
                                 self.set_order_done(code, SELL_CODE)
-                                PRINT_DEBUG(f"[{self.stocks[code]['name']}] 매도 주문, {qty}주 {curr_price}(현재가) {sell_target_price}(목표가)")
+                                PRINT_DEBUG(f"[{self.stocks[code]['name']}] 매도 주문, {qty}주 {curr_price}(현재가) >= {sell_target_price}(목표가)")
                     else:
                         # "현재가 >= 목표가" or "현재가 <= 1차 목표가" 경우 매도
                         if (curr_price >= sell_target_price and sell_target_price > 0) or (curr_price <= self.stocks[code]['first_sell_target_price']):
